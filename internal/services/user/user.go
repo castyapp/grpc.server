2305c1d3a9c60173c821145b208542cc9b6d550b
Alireza Josheghani <josheghani.dev@gmail.com>
2020-08-18 01:07:58 +0430

    Adding updated user's event to friends when user updated

--- internal/services/user/user.go
+++ internal/services/user/user.go
@@ -15,6 +15,36 @@ type InternalWsUserService struct {
 	HttpClient http.Client
 }
 
+func (s *InternalWsUserService) SendUpdateUserEvent(req *proto.AuthenticateRequest, userId string) error {
+
+	params := url.Values{}
+	params.Set("user_id", userId)
+
+	request, err := http.NewRequest("POST", "http://unix/user/@updated", strings.NewReader(params.Encode()))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Authorization", string(req.Token))
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response, err := s.HttpClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	result := map[string] interface{}{}
+	if err := json.NewDecoder(response.Body).Decode(&result); err != nil {
+		return err
+	}
+
+	if result["status"] == "success" {
+		return nil
+	}
+
+	return errors.New("Something went wrong, Could not send event!")
+}
+
 func (s *InternalWsUserService) SendNewNotificationsEvent(req *proto.AuthenticateRequest, userId string) error {
 
 	params := url.Values{}
